ddbbb65a3deb80f7157930c07056bb83d55b98f5
Mauro Gaston Perez Gianolini <pmora92@hotmail.com>
2023-12-26 13:09:03 -0300

    Eliminando logs

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const { startTelegramClient } = require('.');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola desde tu servidor local!');
+  res.send('¡Hola este es el front end!');
 });
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
   startTelegramClient();
+
+  // Enviar un log cada 3 minutos
+  setInterval(() => {
+    console.log('Servidor vivo');
+  }, 180000); // 3 minutos en milisegundos
 });
